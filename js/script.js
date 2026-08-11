5deae397bfebd1fedd85d97e7e29d3fa4cf47fe7
f3llip3 <freirefellipe11@gmail.com>
2026-08-11 15:51:24 -0300

    style:adição melhora do style

--- js/script.js
+++ js/script.js
@@ -13,11 +13,14 @@ btnStatus.forEach((botao, index) => {
             type[index].textContent = `Tipo: ${tipos + tipos2}`;
 
             type[index].style.color = "white";
+            type[index].style.padding = "10px";
+            type[index].style.borderRadius = "8px";
             type[index].style.backgroundColor = "lawngreen";
 
         } else {
 
             type[index].textContent = "";
+            type[index].style.padding = "0px";
 
         }
 
